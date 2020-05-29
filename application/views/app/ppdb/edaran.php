2518d7a64e8d0189b1a1a7b107eb828803668697
insanz01 <insankamil002@gmail.com>
2020-05-29 10:25:08 +0700

    update minor

--- application/views/app/ppdb/edaran.php
+++ application/views/app/ppdb/edaran.php
@@ -3,7 +3,7 @@
     <div class="col-12">
       <h2 class="my-3">Surat Edaran Tahun Ajaran <?= $tahun_satu . ' / ' . $tahun_dua ?></h2>
       <p class="text-justify">Informasi mengenai PPDB bisa diakses pada tautan berikut : </p>
-      <a href="http://localhost:8080/admin_sdn_palampitan/ppdb/edaran/<?= $ppdb['edaran'] ?>"></a>
+      <a href="http://localhost:8080/admin_sdn_palampitan/ppdb/edaran/<?= $ppdb['edaran'] ?>">Unduh di sini</a>
     </div>
   </div>
 </div>
--- application/views/app/ppdb/info.php
+++ application/views/app/ppdb/info.php
@@ -3,7 +3,7 @@
     <div class="col-12">
       <h2 class="my-3">Informasi PPDB Tahun Ajaran <?= $tahun_satu . ' / ' . $tahun_dua ?></h2>
       <p class="text-justify">Informasi mengenai PPDB bisa diakses pada tautan berikut : </p>
-      <a href="http://localhost:8080/admin_sdn_palampitan/dokumen/ppdb/informasi/<?= $ppdb['informasi'] ?>" download>Unduh disini</a>
+      <a href="http://localhost:8080/admin_sdn_palampitan/dokumen/ppdb/informasi/<?= $ppdb['informasi'] ?>" download>Unduh di sini</a>
     </div>
   </div>
 </div>
